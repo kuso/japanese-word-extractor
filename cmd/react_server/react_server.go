5f98a1cc890d9b2049449446f2f5fa155a4e916a
kuso <kuso>
2020-02-17 17:27:03 +0800

    init

--- cmd/react_server/react_server.go
+++ cmd/react_server/react_server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/gzip"
 )
 
 func main() {
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
 
 	// Serve frontend static files
 	//router.Use(static.Serve("/", static.LocalFile("./frontend/build", true)))
