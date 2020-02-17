ee9784051165ca84be5564b7fcb1f82347751d8e
kuso <kuso>
2020-02-17 17:02:16 +0800

    init

---
+++ frontend/build/precache-manifest.ffde786c1ff1d6cc0521f412076102d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27cc798c3c70d380b26793167bb41df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce0fc819b6a7eac4ead",
+    "url": "/static/css/main.fdfa7daa.chunk.css"
+  },
+  {
+    "revision": "053d21fc1179d027e89b",
+    "url": "/static/js/2.fd2e1987.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.fd2e1987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce0fc819b6a7eac4ead",
+    "url": "/static/js/main.e0d749ff.chunk.js"
+  },
+  {
+    "revision": "8a21a9610ca2905bab57",
+    "url": "/static/js/runtime-main.89f24a37.js"
+  }
+]);
